393ad1281b7cc63fea3a664791d8eb6440c9514a
fumiya-810 <fumiyasamo@yahoo.co.jp>
2023-02-14 15:41:42 +0900

    add maps embed api

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
+
 
 class PostController extends Controller
 {
-    public  function index(Post $spot){
-        return view('posts/index')->$spot->get();
+    public function index(Post $spot){
+        return view('posts/index')->with(['spots' => $spot->get()]); 
     }
 }
