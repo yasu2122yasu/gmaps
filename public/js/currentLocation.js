98d216033b69f282c4a67cbb7a15c1997ce1a846
fumiya-810 <fumiyasamo@yahoo.co.jp>
2023-02-22 02:13:35 +0900

    finish 現在地の取得・表示、複数ピンの表示

--- public/js/currentLocation.js
+++ public/js/currentLocation.js
@@ -1,9 +1,39 @@
+var infoWindow=[];
+var marker = [];
+var markerData = [ // マーカーを立てる場所名・緯度・経度
+  {
+       name: 'TAM 東京',
+       lat: 35.6954806,
+        lng: 139.76325010000005,
+ }, {
+        name: '小川町駅',
+     lat: 35.6951212,
+        lng: 139.76610649999998
+ }, {
+        name: '淡路町駅',
+     lat: 35.69496,
+      lng: 139.76746000000003
+ }, {
+        name: '御茶ノ水駅',
+        lat: 35.6993529,
+        lng: 139.76526949999993
+ }, {
+        name: '神保町駅',
+     lat: 35.695932,
+     lng: 139.75762699999996
+ }, {
+        name: '新御茶ノ水駅',
+       lat: 35.696932,
+     lng: 139.76543200000003
+ }
+];
 function initMap() {
+    //現在地の表示
     // google map へ表示するための設定
     latlng = new google.maps.LatLng(lat, lng);
     map = document.getElementById("map");
     opt = {
-        zoom: 2,
+        zoom: 12,
         center: latlng,
     };
     // google map 表示
@@ -13,6 +43,30 @@ function initMap() {
         position: latlng,
         map: mapObj,
         title: '現在地',
+    });
+    
+    //周辺にピンたて
+    for (var i = 0; i < markerData.length; i++) {
+        const image = "https://maps.google.com/mapfiles/ms/icons/yellow-dot.png";
+        markerLatLng = new google.maps.LatLng({lat: markerData[i]['lat'], lng: markerData[i]['lng']}); // 緯度経度のデータ作成
+        marker[i] = new google.maps.Marker({ // マーカーの追加
+         position: markerLatLng, // マーカーを立てる位置を指定
+            map: mapObj, // マーカーを立てる地図を指定
+            icon: image,
+       });
+        //吹き出しの追加
+        infoWindow[i] = new google.maps.InfoWindow({ 
+            content: '<div class="sample">' + markerData[i]['name'] + '</div>' // 吹き出しに表示する内容
+        });
+ 
+        markerEvent(i); // マーカーにクリックイベントを追加
+    }
+}
+
+// マーカーにクリックイベントを追加
+function markerEvent(i) {
+    marker[i].addListener('click', function() { // マーカーをクリックしたとき
+      infoWindow[i].open(map, marker[i]); // 吹き出しの表示
     });
 }
 
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,8 +6,8 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-<!-- maps javascript api テスト -->        
-    <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+        <!-- maps javascript api テスト -->        
+        <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
 
     <!-- cssは正常に読み込めている -->
     <link rel="stylesheet" type="text/css" href="{{ asset('/css/style.css') }}" />
@@ -22,21 +22,21 @@
     <!--現在地取得用に以下を追加　jsファイルの読み込みはヘッダーから移動してきた　-->
    
 	{!! Form::open(['route' => 'posts.currentLocation','method' => 'get']) !!}
-    　　 {{--隠しフォームでresult.currentLocationに位置情報を渡す--}}
-    　　 {{--lat用--}}
-    　　 {!! Form::hidden('lat','lat',['class'=>'lat_input']) !!}
-    　　 {{--lng用--}}
-    　　 {!! Form::hidden('lng','lng',['class'=>'lng_input']) !!}
-    　　 {{--setlocation.jsを読み込んで、位置情報取得するまで押せないようにdisabledを付与し、非アクティブにする。--}}
-    　　 {{--その後、disableはfalseになるようにsetlocation.js内に記述した--}}
-    　　 {!! Form::submit("周辺を表示", ['class' => "btn btn-success btn-block",'disabled']) !!}
+    {{--隠しフォームでresult.currentLocationに位置情報を渡す--}}
+    {{--lat用--}}
+    {!! Form::hidden('lat','lat',['class'=>'lat_input']) !!}
+    {{--lng用--}}
+    {!! Form::hidden('lng','lng',['class'=>'lng_input']) !!}
+    {{--setlocation.jsを読み込んで、位置情報取得するまで押せないようにdisabledを付与し、非アクティブにする。--}}
+    {{--その後、disableはfalseになるようにsetlocation.js内に記述した--}}
+    {!! Form::submit("周辺を表示", ['class' => "btn btn-success btn-block",'disabled']) !!}
     {!! Form::close() !!}
     <!-- jQueryの読み込み-->
     <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
     
     <script type="module" src="{{ asset('/js/index.js')}}"></script>
     <script src="{{ asset('/js/setLocation.js') }}"></script> 
-<!--現在地取得用↑　-->
+    <!--現在地取得用↑　-->
     <script
       src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCPbum-1zCfQ-5MckvI65yLGuBHgvA0w5c&callback=initMap&v=weekly"
       defer
